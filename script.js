dc7194fc043f43bfd2c2e1b9ee9842def0bcbdc1
Tejas Lanjulkar <155477246+TejasLanjulkar@users.noreply.github.com>
2024-10-24 20:04:48 +0530

    Add files via upload
    Sticky-Notes in js

---
+++ script.js
@@ -0,0 +1,51 @@
+let addNote = document.querySelector('.colorContainer button');
+let notesContainer = document.querySelector('.notes');
+let textArea = document.querySelector('textarea');
+let heading = document.querySelector('.down h1');
+let color = document.querySelector('input');
+
+addNote.addEventListener('click', (e) => {
+    //to append the notes
+    let text = textArea.value;
+    heading.style.display = 'none';
+    if(text === ""){
+        e.preventDefault();
+        alert("Please add the text first !");
+        return;
+    }
+    let notes = document.createElement('div');
+    notes.innerHTML = `
+        <div class="note">
+            <p class= "para">
+                ${text}
+            </p>
+            <button btn='del'>Delete</button>
+        </div>
+    `
+    notes.classList.add('.note');
+    notesContainer.appendChild(notes);
+
+    let stickyNoteContent = document.querySelectorAll('.note p');
+    stickyNoteContent.forEach((eachNote) => {
+        eachNote.style.backgroundColor= color.value;
+    });
+
+    let stickyNote = document.querySelectorAll('.note');
+    stickyNote.forEach((eachNote) => {
+        eachNote.style.backgroundColor= color.value;
+    });
+
+    textArea.value = "";
+
+     //to delete the notes
+    let delbutton = document.querySelectorAll(`button[btn="del"]`);
+    delbutton.forEach((dltEle) => {
+        dltEle.addEventListener('click' , (e) => {
+            if(dltEle.innerText == 'Delete'){
+                e.target.parentElement.parentElement.remove();
+            }
+        });
+        
+    });   
+
+});
